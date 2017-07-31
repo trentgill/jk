72f831fdcc18ff8b301374144468f6aca6c41fb6
Trent <trent.gill@gmail.com>
2017-07-31 10:55:31 -0400

    full rewrite using libsoundio as driver instead of jack natively. naive wrapper around the module process frame with hardwired support for floats only. no input from soundcard yet

--- main.c
+++ main.c
@@ -1,198 +1,317 @@
-// standard C libs
+/*
+ * Copyright (c) 2015 Andrew Kelley
+ *
+ * This file is part of libsoundio, which is MIT licensed.
+ * See http://opensource.org/licenses/MIT
+ */
+
+#include <soundio/soundio.h>
+
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <stdint.h>
 #include <math.h>
-#include <string.h> /* for atoi */
 
-#include <jack/jack.h> // Jack headers
-// #include <lo/lo.h> // Liblo headers
-
-// File headers
 #include "main.h"
 
-// private variables
-jack_client_t *client;
-
-jack_default_audio_sample_t *delay_line;
-jack_nframes_t delay_index;
-jack_nframes_t latency = 1024;
-
-jack_port_t *input_ports[IN_PORTS];
-jack_port_t *output_ports[OUT_PORTS];
-
-
-// private fns
-
-int32_t process_block (jack_nframes_t nframes, void *arg)
-{
-	// audio i/o buffers
-	jack_default_audio_sample_t* jack_in[IN_PORTS];
-	jack_default_audio_sample_t* jack_out[OUT_PORTS];
-	int32_t ch;
-	
-	// get input buffers from jack (nb: reverse 'output' symbol)
-	for (ch=0; ch < IN_PORTS; ch++) {
-		jack_out[ch] = jack_port_get_buffer (output_ports[ch], nframes);
-	}
-
-	// get an output buffers (reserved memory) to send output back to jack
-	for (ch=0; ch < OUT_PORTS; ch++) {
-		jack_in[ch] = jack_port_get_buffer (input_ports[ch], nframes);
-	}
-
-	// NB: THESE FNS ARE SUPER WEIRD, THEY SEND LEFT THEN RIGHT CHANS
-	// IN SEQUENCE, RATHER THAN AS A PAIR. HOW TO DO THIS IN A GENERIC WAY?
-	// PERHAPS JUST SEND A POINTER TO THE BEGINNING OF THE FRAME BLOCKS.
-	// THIS IS WHAT A CODEC DOES, SO YOU NEED THE SAME SERIALIZATION GLUE.
-
-	// NEED TO SETTLE ON A GENERIC FORM SO IT CAN BE ROLLED INTO
-	// PRODUCTION CODE WITHOUT REQUIRING MODIFICATION BETWEEN SIM & SHIP
-
-
-#ifdef SINGLE_SAMPLE /* single-sample processing */
-	for (int32_t i=0; i < nframes; i++) {
-		for (ch=0; ch < IN_PORTS; ch++) {
-			// module_process_frame is extern
-			// define it in your dsp code!
-			*(jack_out[ch] + i) = module_process_frame( *(jack_in[ch] + i) );
-		}
-	}
-#else /* native block-processing */
-	for (ch=0; ch < IN_PORTS; ch++) {
-		// module_process_frame is extern
-		// define it in your dsp code
-		module_process_frame( jack_in[ch], jack_out[ch], nframes );
-	}
-#endif /* end of block process */
-
-	return 0;
+static int usage(char *exe) {
+    fprintf(stderr, "Usage: %s [options]\n"
+            "Options:\n"
+            "  [--backend dummy|alsa|pulseaudio|jack|coreaudio|wasapi]\n"
+            "  [--device id]\n"
+            "  [--raw]\n"
+            "  [--name stream_name]\n"
+            "  [--latency seconds]\n"
+            "  [--sample-rate hz]\n"
+            , exe);
+    return 1;
 }
 
-void jack_shutdown (void *arg)
-{
-	fprintf(stderr, "JACK shut down, exiting ...\n");
-	exit (1);
+static void write_sample_s16ne(char *ptr, double sample) {
+    int16_t *buf = (int16_t *)ptr;
+    double range = (double)INT16_MAX - (double)INT16_MIN;
+    double val = sample * range / 2.0;
+    *buf = val;
 }
 
-// private declarations
-/*int32_t foo_handler(const uint8_t *path, const uint8_t *types, lo_arg ** argv,
-						int32_t argc, void *data, void *user_data);
-int32_t generic_handler(const uint8_t *path, const uint8_t *types, lo_arg ** argv,
-						int32_t argc, void *data, void *user_data);*/
-void error(int32_t num, const uint8_t *m, const uint8_t *path);
-
-// exported fns
-int32_t main (int32_t argc, uint8_t *argv[])
-{
-	const char **ports;
-	const char *client_name = "jkkk";
-	const char *server_name = NULL;
-
-	
-	jack_options_t options = JackNullOption;
-	jack_status_t status;
-
-	// use jack latency to create a sample buffer
-	if (argc == 2) { latency = atoi(argv[1]); }
-	delay_line = malloc( latency * sizeof(jack_default_audio_sample_t));
-	if (delay_line == NULL) { fprintf (stderr, "no memory"); exit(1); }
-	memset( delay_line, 0, latency * sizeof(jack_default_audio_sample_t));
-
-	// open a client connection to the JACK server
-	client = jack_client_open (client_name, options, &status, server_name);
-	printf("START!\n");
-	if (client == NULL) {
-		fprintf (stderr, "jack_client_open() failed, status = 0x%2.0x\n", status);
-	    if (status & JackServerFailed) {
-			fprintf (stderr, "Unable to connect to JACK server\n");
-		}
-		exit (1);
-	}
-	if (status & JackServerStarted) { fprintf (stderr, "JACK server started\n"); }
-	if (status & JackNameNotUnique) {
-		client_name = jack_get_client_name(client);
-		fprintf (stderr, "unique name `%s' assigned\n", client_name);
-	}
-
-	// Tell Jack server to run process_block() when there's work to be done
-	jack_set_process_callback (client, process_block, 0);
-
-    // tell Jack server to call jack_shutdown() if it needs to exit
-	jack_on_shutdown (client, jack_shutdown, 0);
-
-	printf ("engine sample rate: %" PRIu32 "\n", jack_get_sample_rate (client));
-
-  /* create the ports */
-	// create Jack ports
-	int32_t j;
-	for (j=0; j < IN_PORTS; j++) {
-		uint8_t foo[100]; // str_buf
-		sprintf(foo, "input%d", j);
-		input_ports[j] = jack_port_register (client, foo,
-							JACK_DEFAULT_AUDIO_TYPE,
-							JackPortIsInput, 0);
-		if (input_ports[j] == NULL) {
-			fprintf(stderr, "no more JACK ports available\n");
-			exit (1);
-		}
-	}
-	for (j=0; j < IN_PORTS; j++) {
-		uint8_t foo[100];
-		sprintf(foo, "output%d", j);
-		output_ports[j] = jack_port_register (client, foo,
-							JACK_DEFAULT_AUDIO_TYPE,
-							JackPortIsOutput, 0);
-		if (output_ports[j] == NULL) {
-			fprintf(stderr, "no more JACK ports available\n");
-			exit (1);
-		}
-	}
-
-	if (jack_activate (client)) {
-		fprintf (stderr, "cannot activate client");
-		exit (1);
-	}
-
-/*#ifdef WIN32
-	Sleep (-1);
-#else
-	sleep (-1);
-#endif
-*/
-	/* this is never reached but if the program
-		had some other way to exit besides being killed,
-		they would be important to call.
-	*/
-
-	jack_client_close (client);
-	exit (0);
+static void write_sample_s32ne(char *ptr, double sample) {
+    int32_t *buf = (int32_t *)ptr;
+    double range = (double)INT32_MAX - (double)INT32_MIN;
+    double val = sample * range / 2.0;
+    *buf = val;
 }
 
-/*int32_t generic_handler(const uint8_t *path, const uint8_t *types, lo_arg ** argv,
-						int32_t argc, void *data, void *user_data)
-{
-	printf("path: <%s>\n", path);
-	for (int32_t i = 0; i < argc; i++) {
-		printf("arg %d '%c' ", i, types[i]);
-		lo_arg_pp((lo_type)types[i], argv[i]);
-		printf("\n");
-	}
-	printf("\n");
-	fflush(stdout);
-
-	return 1;
+static void write_sample_float32ne(char *ptr, double sample) {
+    float *buf = (float *)ptr;
+    *buf = sample;
 }
 
-int32_t foo_handler(const uint8_t *path, const uint8_t *types, lo_arg ** argv,
-					int32_t argc, void *data, void *user_data) {
-	printf("received param %d %d\n", argv[0]->i, argv[1]->i);
-	module_set_param(argv[0]->i, argv[1]->i);
-	return 0;
-}*/
+static void write_sample_float64ne(char *ptr, double sample) {
+    double *buf = (double *)ptr;
+    *buf = sample;
+}
 
+static void (*write_sample)(char *ptr, double sample);
+static const double PI = 3.14159265358979323846264338328;
+static double seconds_offset = 0.0;
+static volatile bool want_pause = false;
+static void write_callback(struct SoundIoOutStream *outstream, int frame_count_min, int frame_count_max) {
+    double float_sample_rate = outstream->sample_rate;
+    double seconds_per_frame = 1.0 / float_sample_rate;
+    struct SoundIoChannelArea *areas;
+    int err;
 
-void error(int num, const uint8_t *msg, const uint8_t *path)
-{
-	printf("liblo server error %d in path %s: %s\n", num, path, msg);
-	fflush(stdout);
+    int frames_left = frame_count_max;
+
+    for (;;) {
+        int frame_count = frames_left;
+        if ((err = soundio_outstream_begin_write(outstream, &areas, &frame_count))) {
+            fprintf(stderr, "unrecoverable stream error: %s\n", soundio_strerror(err));
+            exit(1);
+        }
+
+        if (!frame_count)
+            break;
+
+        const struct SoundIoChannelLayout *layout = &outstream->layout;
+
+        double pitch = 440.0;
+        double radians_per_second = pitch * 2.0 * PI;
+#ifdef SINGLE_SAMPLE /* sample-by-sample */
+        for (int frame=0; frame < frame_count; frame++){
+            for (int channel=0; channel<layout->channel_count; channel++){
+                write_sample( areas[channel].ptr, module_process_frame( NULL ) );
+                areas[channel].ptr += areas[channel].step;
+            }
+        }
+#else /* block process  */
+        // nb: these should be doubles for desktop applications
+        // assuming a single channel rn
+        float in[frame_count];
+        float out[frame_count];
+        // read_sample into the in[] buffer
+
+        module_process_frame( in, out, frame_count );
+
+        // nb: need to wrap channels here as well
+        for( int frame=0; frame < frame_count; frame++ ){
+            for(int channel=0; channel<layout->channel_count; channel++){
+                write_sample( areas[channel].ptr, out[frame] );
+                areas[channel].ptr += areas[channel].step;
+            }
+        }
+
+#endif /* end sample process */
+
+        for (int frame = 0; frame < frame_count; frame += 1) {
+            double sample = sin((seconds_offset + frame * seconds_per_frame) * radians_per_second);
+            for (int channel = 0; channel < layout->channel_count; channel += 1) {
+                write_sample(areas[channel].ptr, sample);
+                areas[channel].ptr += areas[channel].step;
+            }
+        }
+        seconds_offset = fmod(seconds_offset + seconds_per_frame * frame_count, 1.0);
+
+        if ((err = soundio_outstream_end_write(outstream))) {
+            if (err == SoundIoErrorUnderflow)
+                return;
+            fprintf(stderr, "unrecoverable stream error: %s\n", soundio_strerror(err));
+            exit(1);
+        }
+
+        frames_left -= frame_count;
+        if (frames_left <= 0)
+            break;
+    }
+
+    soundio_outstream_pause(outstream, want_pause);
+}
+
+static void underflow_callback(struct SoundIoOutStream *outstream) {
+    static int count = 0;
+    fprintf(stderr, "underflow %d\n", count++);
+}
+
+int main(int argc, char **argv) {
+    char *exe = argv[0];
+    enum SoundIoBackend backend = SoundIoBackendNone;
+    char *device_id = NULL;
+    bool raw = false;
+    char *stream_name = NULL;
+    double latency = 0.0;
+    int sample_rate = 0;
+    for (int i = 1; i < argc; i += 1) {
+        char *arg = argv[i];
+        if (arg[0] == '-' && arg[1] == '-') {
+            if (strcmp(arg, "--raw") == 0) {
+                raw = true;
+            } else {
+                i += 1;
+                if (i >= argc) {
+                    return usage(exe);
+                } else if (strcmp(arg, "--backend") == 0) {
+                    if (strcmp(argv[i], "dummy") == 0) {
+                        backend = SoundIoBackendDummy;
+                    } else if (strcmp(argv[i], "alsa") == 0) {
+                        backend = SoundIoBackendAlsa;
+                    } else if (strcmp(argv[i], "pulseaudio") == 0) {
+                        backend = SoundIoBackendPulseAudio;
+                    } else if (strcmp(argv[i], "jack") == 0) {
+                        backend = SoundIoBackendJack;
+                    } else if (strcmp(argv[i], "coreaudio") == 0) {
+                        backend = SoundIoBackendCoreAudio;
+                    } else if (strcmp(argv[i], "wasapi") == 0) {
+                        backend = SoundIoBackendWasapi;
+                    } else {
+                        fprintf(stderr, "Invalid backend: %s\n", argv[i]);
+                        return 1;
+                    }
+                } else if (strcmp(arg, "--device") == 0) {
+                    device_id = argv[i];
+                } else if (strcmp(arg, "--name") == 0) {
+                    stream_name = argv[i];
+                } else if (strcmp(arg, "--latency") == 0) {
+                    latency = atof(argv[i]);
+                } else if (strcmp(arg, "--sample-rate") == 0) {
+                    sample_rate = atoi(argv[i]);
+                } else {
+                    return usage(exe);
+                }
+            }
+        } else {
+            return usage(exe);
+        }
+    }
+
+    struct SoundIo *soundio = soundio_create();
+    if (!soundio) {
+        fprintf(stderr, "out of memory\n");
+        return 1;
+    }
+
+    int err = (backend == SoundIoBackendNone) ?
+        soundio_connect(soundio) : soundio_connect_backend(soundio, backend);
+
+    if (err) {
+        fprintf(stderr, "Unable to connect to backend: %s\n", soundio_strerror(err));
+        return 1;
+    }
+
+    fprintf(stderr, "Backend: %s\n", soundio_backend_name(soundio->current_backend));
+
+    soundio_flush_events(soundio);
+
+    int selected_device_index = -1;
+    if (device_id) {
+        int device_count = soundio_output_device_count(soundio);
+        for (int i = 0; i < device_count; i += 1) {
+            struct SoundIoDevice *device = soundio_get_output_device(soundio, i);
+            bool select_this_one = strcmp(device->id, device_id) == 0 && device->is_raw == raw;
+            soundio_device_unref(device);
+            if (select_this_one) {
+                selected_device_index = i;
+                break;
+            }
+        }
+    } else {
+        selected_device_index = soundio_default_output_device_index(soundio);
+    }
+
+    if (selected_device_index < 0) {
+        fprintf(stderr, "Output device not found\n");
+        return 1;
+    }
+
+    struct SoundIoDevice *device = soundio_get_output_device(soundio, selected_device_index);
+    if (!device) {
+        fprintf(stderr, "out of memory\n");
+        return 1;
+    }
+
+    fprintf(stderr, "Output device: %s\n", device->name);
+
+    if (device->probe_error) {
+        fprintf(stderr, "Cannot probe device: %s\n", soundio_strerror(device->probe_error));
+        return 1;
+    }
+
+    struct SoundIoOutStream *outstream = soundio_outstream_create(device);
+    if (!outstream) {
+        fprintf(stderr, "out of memory\n");
+        return 1;
+    }
+
+    outstream->write_callback = write_callback;
+    outstream->underflow_callback = underflow_callback;
+    outstream->name = stream_name;
+    outstream->software_latency = latency;
+    outstream->sample_rate = sample_rate;
+
+    if (soundio_device_supports_format(device, SoundIoFormatFloat32NE)) {
+        outstream->format = SoundIoFormatFloat32NE;
+        write_sample = write_sample_float32ne;
+    } else if (soundio_device_supports_format(device, SoundIoFormatFloat64NE)) {
+        outstream->format = SoundIoFormatFloat64NE;
+        write_sample = write_sample_float64ne;
+    } else if (soundio_device_supports_format(device, SoundIoFormatS32NE)) {
+        outstream->format = SoundIoFormatS32NE;
+        write_sample = write_sample_s32ne;
+    } else if (soundio_device_supports_format(device, SoundIoFormatS16NE)) {
+        outstream->format = SoundIoFormatS16NE;
+        write_sample = write_sample_s16ne;
+    } else {
+        fprintf(stderr, "No suitable device format available.\n");
+        return 1;
+    }
+
+    if ((err = soundio_outstream_open(outstream))) {
+        fprintf(stderr, "unable to open device: %s", soundio_strerror(err));
+        return 1;
+    }
+
+    fprintf(stderr, "Software latency: %f\n", outstream->software_latency);
+    fprintf(stderr,
+            "'p\\n' - pause\n"
+            "'u\\n' - unpause\n"
+            "'P\\n' - pause from within callback\n"
+            "'c\\n' - clear buffer\n"
+            "'q\\n' - quit\n");
+
+    if (outstream->layout_error)
+        fprintf(stderr, "unable to set channel layout: %s\n", soundio_strerror(outstream->layout_error));
+
+    if ((err = soundio_outstream_start(outstream))) {
+        fprintf(stderr, "unable to start device: %s\n", soundio_strerror(err));
+        return 1;
+    }
+
+    for (;;) {
+        soundio_flush_events(soundio);
+        int c = getc(stdin);
+        if (c == 'p') {
+            fprintf(stderr, "pausing result: %s\n",
+                    soundio_strerror(soundio_outstream_pause(outstream, true)));
+        } else if (c == 'P') {
+            want_pause = true;
+        } else if (c == 'u') {
+            want_pause = false;
+            fprintf(stderr, "unpausing result: %s\n",
+                    soundio_strerror(soundio_outstream_pause(outstream, false)));
+        } else if (c == 'c') {
+            fprintf(stderr, "clear buffer result: %s\n",
+                    soundio_strerror(soundio_outstream_clear_buffer(outstream)));
+        } else if (c == 'q') {
+            break;
+        } else if (c == '\r' || c == '\n') {
+            // ignore
+        } else {
+            fprintf(stderr, "Unrecognized command: %c\n", c);
+        }
+    }
+
+    soundio_outstream_destroy(outstream);
+    soundio_device_unref(device);
+    soundio_destroy(soundio);
+    return 0;
 }
