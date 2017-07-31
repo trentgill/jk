b21531a3d64b36377f914a2a130d9524a2100a8a
Trent <trent.gill@gmail.com>
2017-07-31 10:54:18 -0400

    example function now creates white noise with libc rand() fn

--- dsp_block.c
+++ dsp_block.c
@@ -1,15 +1,18 @@
 #include "dsp_block.h"
+#include <stdlib.h>
 
 #ifdef SINGLE_SAMPLE
 float module_process_frame(float in)
 {
-	return in; // echo
+    return ((float)rand()) / (float)0x3FFFFFFF - 1.0;
 }
 #else
 void module_process_frame(float* in, float* out, uint16_t b_size)
 {
 	for(uint16_t i=0; i< b_size; i++){
-		*out++ = *in++; // echo
+        float rando = ((float)rand()) / (float)0x3FFFFFFF - 1.0;
+        *out++ = rando;
 	}
 }
 #endif
+
