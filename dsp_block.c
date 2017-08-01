c37e221b3b747b0b2bd144b961a6c5135a6fd546
Trent <trent.gill@gmail.com>
2017-07-31 22:35:55 -0400

    include wrMath and use a vector multiply for proof

--- dsp_block.c
+++ dsp_block.c
@@ -1,6 +1,8 @@
 #include "dsp_block.h"
 #include <stdlib.h>
 
+#include "wrMath.h"
+
 #ifdef SINGLE_SAMPLE
 float module_process_frame(float in)
 {
@@ -13,6 +15,7 @@ void module_process_frame(float* in, float* out, uint16_t b_size)
         float rando = ((float)rand()) / (float)0x3FFFFFFF - 1.0;
         *out++ = rando;
 	}
+    mul_vf_f(out, 0.5, out, b_size);
 }
 #endif
 
