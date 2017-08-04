4b026ea6ca08311e8854311b31be489adb805878
Trent <trent.gill@gmail.com>
2017-08-04 17:06:08 -0400

    3 sinewaves into a filter

--- dsp_block.c
+++ dsp_block.c
@@ -3,27 +3,54 @@
 
 #include "wrMath.h"
 #include "wrFilter.h"
+#include "wrOscSine.h"
 
 filter_lp1_t filt;
+osc_sine_t   sineo[3];
 
 void module_init( void )
 {
- //   lp1_init( &filt );
-  //  lp1_set_coeff( &filt, 0.0001 );
+    for(uint8_t p=0;p<3;p++){
+        osc_sine_init( &sineo[p] );
+        float tt = ((float)(p)*1.5 + 1.0) * 0.02;
+        osc_sine_time( &sineo[p], tt );
+    }
+    lp1_init( &filt );
+    lp1_set_coeff( &filt, 0.0001 );
 }
 
+#ifdef SINGLE_SAMPLE
 void module_process_frame(float* in, float* out, uint16_t b_size)
 {
-#ifdef SINGLE_SAMPLE
     *out++ = 0.0;
-#else
-    for(uint16_t i=0; i< b_size; i++){
-        float rando = ((float)rand()) / (float)0x3FFFFFFF - 1.0;
-        *out++ = rando;
-    }
-    float tmp[b_size];
-    mul_vf_f(out, 0.5, tmp, b_size);
-    lp1_step_v( &filt, tmp, out, b_size );
-#endif
 }
+#else
+void module_process_frame(float* in, float* out, uint16_t b_size)
+{
+    float  tmp[b_size];
+    float  tmpx[b_size];
+    float* out2 = out;
+    float* tmp2 = tmp;
+    float* tmpx2 = tmpx;
+    uint16_t i;
+    for( i=0; i<b_size; i++ ){
+        *tmpx2++ = 0.0;
+        *tmp2++ = 0.0;
+        *out2++ = 0.0;
+    }
+    for( uint16_t p=0; p<3; p++ ){
+        out2 = out;
+        tmp2 = tmp;
+        osc_sine_process_v( &sineo[p]
+            , b_size
+            , tmpx
+            , tmp );
+        for( i=0; i<b_size; i++ ){
+            *out2++ += *tmp2++;
+        }
+    }
+    mul_vf_f(out, 0.33, tmp, b_size);
+    lp1_step_v( &filt, tmp, out, b_size );
+}
+#endif
 
